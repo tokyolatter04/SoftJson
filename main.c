04b3791d49665614616dccd126195740a4ee78be
tokyolatter <97055625+tokyolatter00@users.noreply.github.com>
2023-03-19 20:18:22 +0000

    Added main.c file

---
+++ main.c
@@ -0,0 +1,28 @@
+
+#include "include/softjson.h"
+#include "tests.h"
+#include "examples.h"
+
+int main(void) {
+
+	// Create animal
+
+	JsonObject animal = json_create_object();
+
+	json_object_add(&animal, "name", json_create_string_value("Polar Bear"));
+	json_object_add(&animal, "age", json_create_int_value(22));
+	json_object_add(&animal, "weight-kg", json_create_int_value(1130));
+	json_object_add(&animal, "is-mammal", json_create_bool_value(TRUE));
+
+	// Output animal
+
+	JsonHandler handler = soft_create_handler();
+	char* serial = soft_dump_string(&handler, json_create_object_value(animal));
+
+	// Data: {"name":"Polar Bear","age":22,"weight-kg":1130,"is-mammal":true}
+	printf("Data: %s\n", serial);
+
+	// Free data
+
+	json_object_free(&animal, FALSE);
+}
